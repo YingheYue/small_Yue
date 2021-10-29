8d1ef7e128aff540f30e004d105c96f7028ee2a9
YingheYue <yingheyue@gmail.com>
2021-10-30 02:37:48 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,9 @@
 import discord
 from discord.ext import commands
 
-bot = commands.bot(command_prefix='!y')
+bot = commands.Bot(command_prefix='!y')
 
 @bot.event
 async def on_ready():
-    print(">>Bot is online<<")
-
+    print(">>bot is online<<")
 bot.run('OTAzNjc5MDk2MjkzMjk0MTQw.YXwevg.K9CkFX8ZWiM4vWvov7obXfyYNfk')
